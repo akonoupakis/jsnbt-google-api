28c7c9c00b22800c2309bb21ea7b94b1a3249c70
Apostolis Konoupakis <konoupakis@gmail.com>
2015-10-03 09:27:31 +0300

    rootScope arguments

--- src/web/admin/js/gApi/app/controllers/GApiController.js
+++ src/web/admin/js/gApi/app/controllers/GApiController.js
@@ -1,14 +1,14 @@
 ﻿; (function () {
     "use strict";
 
-    var SettingsController = function ($scope, $route, $location, $jsnbt) {
-        jsnbt.controllers.SettingsControllerBase.apply(this, $scope.getBaseArguments($scope));
+    var SettingsController = function ($scope, $rootScope, $route, $location, $jsnbt) {
+        jsnbt.controllers.SettingsControllerBase.apply(this, $rootScope.getBaseArguments($scope));
         
         $scope.init();
     };
     SettingsController.prototype = Object.create(jsnbt.controllers.SettingsControllerBase.prototype);
 
     angular.module("jsnbt-google-api")
-        .controller('GApiController', ['$scope', '$route', '$location', '$jsnbt', SettingsController]);
+        .controller('GApiController', ['$scope', '$rootScope', '$route', '$location', '$jsnbt', SettingsController]);
 
 })();
